ff358d17f8a3ba9c70ac641871d771256548f0e0
not-celeska <bushkov.mikhail@gmail.com>
2023-12-27 21:53:58 -0500

    added submit data and button!

--- app/src/main/java/com/example/multiactivitytest/RightTriangleActivity.java
+++ app/src/main/java/com/example/multiactivitytest/RightTriangleActivity.java
@@ -4,13 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RightTriangleActivity extends AppCompatActivity {
+
+    double sideA, sideB, sideC, angleA, angleB;
+    TextView info;
+    EditText fieldAngleB;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_right_triangle);
+
+        // initialize views.
+        info = findViewById(R.id.inputtedData);
+        fieldAngleB = findViewById(R.id.angleB);
+    }
+
+    public void submitData(View view) {
+        String inputText = fieldAngleB.getText().toString().trim();
+
+        if (!inputText.isEmpty()) {
+            info.setText("Angle B input! --> " + Double.valueOf(inputText));
+        } else {
+            // Handle the case where nothing is entered
+            info.setText("Please enter a value for Angle B.");
+        }
     }
 
     public void closeActivity(View view)
